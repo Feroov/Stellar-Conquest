a02e484599fe09543ffbecde71e37a039c361eac
Feroov <feroovt@gmail.com>
2023-05-01 23:44:21 +0200

    Register ItemsSTLCON & Admin sword

--- src/main/java/com/feroov/frv/STLCON.java
+++ src/main/java/com/feroov/frv/STLCON.java
@@ -1,6 +1,8 @@
 package com.feroov.frv;
 
 
+import com.feroov.frv.item.ItemsSTLCON;
+import com.feroov.frv.item.TabsSTLCON;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,11 +18,19 @@ public class STLCON
 
     public STLCON()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ItemsSTLCON.register(eventBus);
+
+        eventBus.addListener(this::addCreative);
         MinecraftForge.EVENT_BUS.register(this);
-        modEventBus.addListener(this::addCreative);
     }
 
-    private void addCreative(CreativeModeTabEvent.BuildContents event) {}
+    private void addCreative(CreativeModeTabEvent.BuildContents event)
+    {
+        if(event.getTab() == TabsSTLCON.STLCON_TAB)
+        {
+            event.accept(ItemsSTLCON.ADMIN_SWORD);
+        }
+    }
 }
