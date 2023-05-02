f99db4629208233f2408c602ecdb6899a3f92d55
Feroov <feroovt@gmail.com>
2023-05-02 23:19:23 +0200

    Event handling for Raygun Beam entity

---
+++ src/main/java/com/feroov/frv/events/ClientModEvents.java
@@ -0,0 +1,22 @@
+package com.feroov.frv.events;
+
+
+import com.feroov.frv.STLCON;
+import com.feroov.frv.entity.EntitiesSTLCON;
+import com.feroov.frv.entity.projectile.renderer.RaygunBeamRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = STLCON.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientModEvents
+{
+    public ClientModEvents(){}
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event)
+    {
+        event.registerEntityRenderer(EntitiesSTLCON.RAYGUN_BEAM.get(), RaygunBeamRenderer::new);
+    }
+}
