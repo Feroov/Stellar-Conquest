174a70468611eb440e3940dd6ecf00adf46b07a8
Feroov <feroovt@gmail.com>
2023-05-06 22:34:16 +0200

    Refactor name error

--- src/main/java/com/feroov/frv/sound/SoundEventsSTLCON.java
+++ src/main/java/com/feroov/frv/sound/SoundEventsSTLCON.java
@@ -12,7 +12,7 @@ public class SoundEventsSTLCON
 {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, STLCON.MOD_ID);
 
-    public static final RegistryObject<SoundEvent> RATGUN_SHOOT = SOUND_EVENTS.register("raygun_shoot", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(STLCON.MOD_ID, "raygun_shoot")));
+    public static final RegistryObject<SoundEvent> RAYGUN_SHOOT = SOUND_EVENTS.register("raygun_shoot", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(STLCON.MOD_ID, "raygun_shoot")));
 
     public static void register(IEventBus eventBus) { SOUND_EVENTS.register(eventBus); }
 }
