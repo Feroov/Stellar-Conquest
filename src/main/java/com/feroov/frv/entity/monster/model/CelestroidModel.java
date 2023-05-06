6847f4d3f63c96434b25d10859210d5fd3cc245e
Feroov <feroovt@gmail.com>
2023-05-06 22:34:34 +0200

    Celestroid model and renderer

---
+++ src/main/java/com/feroov/frv/entity/monster/model/CelestroidModel.java
@@ -0,0 +1,45 @@
+package com.feroov.frv.entity.monster.model;
+
+import com.feroov.frv.STLCON;
+import com.feroov.frv.entity.monster.Celestroid;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.model.data.EntityModelData;
+
+public class CelestroidModel extends GeoModel<Celestroid>
+{
+    @Override
+    public ResourceLocation getModelResource(Celestroid animatable)
+    {
+        return new ResourceLocation(STLCON.MOD_ID, "geo/celestroid.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureResource(Celestroid animatable)
+    {
+        return new ResourceLocation(STLCON.MOD_ID, "textures/entity/celestroid.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationResource(Celestroid animatable)
+    {
+        return new ResourceLocation(STLCON.MOD_ID, "animations/celestroid.json");
+    }
+
+    @Override
+    public void setCustomAnimations(Celestroid animatable, long instanceId, AnimationState<Celestroid> animationState)
+    {
+        CoreGeoBone head = getAnimationProcessor().getBone("head");
+
+        if (head != null) {
+            EntityModelData entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA);
+
+            head.setRotX(entityData.headPitch() * Mth.DEG_TO_RAD);
+            head.setRotY(entityData.netHeadYaw() * Mth.DEG_TO_RAD);
+        }
+    }
+}
---
+++ src/main/java/com/feroov/frv/entity/monster/renderer/CelestroidRenderer.java
@@ -0,0 +1,45 @@
+package com.feroov.frv.entity.monster.renderer;
+
+import com.feroov.frv.STLCON;
+import com.feroov.frv.entity.monster.Celestroid;
+import com.feroov.frv.entity.monster.model.CelestroidModel;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+
+public class CelestroidRenderer extends GeoEntityRenderer<Celestroid>
+{
+    public CelestroidRenderer(EntityRendererProvider.Context renderManager)
+    {
+        super(renderManager, new CelestroidModel());
+    }
+
+    @Override
+    public @NotNull ResourceLocation getTextureLocation(@NotNull Celestroid animatable)
+    {
+        return new ResourceLocation(STLCON.MOD_ID, "textures/entity/celestroid.png");
+    }
+
+    @Override
+    public void render(@NotNull Celestroid entity, float entityYaw, float partialTick, @NotNull PoseStack poseStack,
+                       @NotNull MultiBufferSource bufferSource, int packedLight)
+    {
+        super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
+    }
+
+    @Override
+    protected float getDeathMaxRotation(Celestroid entityLivingBaseIn)
+    {
+        return 0;
+    }
+
+
+    @Override
+    protected int getBlockLightLevel(@NotNull Celestroid entity, @NotNull BlockPos blockPos) {
+        return 15;
+    }
+}
