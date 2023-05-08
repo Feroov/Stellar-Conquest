87230f5d08f14491b4069f21282aa4d40dcf0f54
Feroov <feroovt@gmail.com>
2023-05-08 17:36:12 +0200

    Refactor addCreative method into TabsSTLCON

--- src/main/java/com/feroov/frv/item/TabsSTLCON.java
+++ src/main/java/com/feroov/frv/item/TabsSTLCON.java
@@ -5,10 +5,10 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
 
 @Mod.EventBusSubscriber(modid = STLCON.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TabsSTLCON
@@ -20,5 +20,15 @@ public class TabsSTLCON
         STLCON_TAB = event.registerCreativeModeTab(new ResourceLocation(STLCON.MOD_ID, "stlcon_tab"),
                 builder -> builder.icon(() -> new ItemStack(ItemsSTLCON.COSMIC_RAY_GUN.get()))
                         .title(Component.translatable("creativemodetab.stlcon_tab")));
+    }
+
+    @SubscribeEvent
+    public static void registerCreativeModeTabs(CreativeModeTabEvent.BuildContents event) {
+        if(event.getTab() == TabsSTLCON.STLCON_TAB)
+        {
+            event.accept(ItemsSTLCON.ADMIN_SWORD);
+            event.accept(ItemsSTLCON.COSMIC_RAY_GUN);
+            event.accept(ItemsSTLCON.CELESTROID_SPAWN_EGG);
+        }
     }
 }
