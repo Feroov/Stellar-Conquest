6b8f5f6549c88467cd1469408078a67a0976bd7b
Feroov <feroovt@gmail.com>
2023-05-08 17:35:49 +0200

    Remove addCreative method

--- src/main/java/com/feroov/frv/STLCON.java
+++ src/main/java/com/feroov/frv/STLCON.java
@@ -3,11 +3,9 @@ package com.feroov.frv;
 
 import com.feroov.frv.entity.EntitiesSTLCON;
 import com.feroov.frv.item.ItemsSTLCON;
-import com.feroov.frv.item.TabsSTLCON;
 import com.feroov.frv.item.RangedItems;
 import com.feroov.frv.sound.SoundEventsSTLCON;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -28,7 +26,6 @@ public class STLCON
         EntitiesSTLCON.register(eventBus);
         ItemsSTLCON.register(eventBus);
 
-        eventBus.addListener(this::addCreative);
         eventBus.addListener(this::doClientStuff);
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -36,15 +33,5 @@ public class STLCON
     private void doClientStuff(final FMLClientSetupEvent event)
     {
         RangedItems.addRanged();
-    }
-
-    private void addCreative(CreativeModeTabEvent.BuildContents event)
-    {
-        if(event.getTab() == TabsSTLCON.STLCON_TAB)
-        {
-            event.accept(ItemsSTLCON.ADMIN_SWORD);
-            event.accept(ItemsSTLCON.COSMIC_RAY_GUN);
-            event.accept(ItemsSTLCON.CELESTROID_SPAWN_EGG);
-        }
     }
 }
