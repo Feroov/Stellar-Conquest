a500695f88e123248f691e983d6c22526f303309
Feroov <feroovt@gmail.com>
2023-05-10 22:59:42 +0200

    Mothership size change

--- src/main/java/com/feroov/frv/entity/monster/renderer/MothershipRenderer.java
+++ src/main/java/com/feroov/frv/entity/monster/renderer/MothershipRenderer.java
@@ -3,12 +3,15 @@ package com.feroov.frv.entity.monster.renderer;
 import com.feroov.frv.STLCON;
 import com.feroov.frv.entity.monster.Mothership;
 import com.feroov.frv.entity.monster.model.MothershipModel;
+import com.feroov.frv.entity.projectile.MothershipBeam;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class MothershipRenderer extends GeoEntityRenderer<Mothership>
@@ -30,6 +33,16 @@ public class MothershipRenderer extends GeoEntityRenderer<Mothership>
     {
         super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
     }
+
+    @Override
+    public void preRender(PoseStack poseStack, Mothership animatable, BakedGeoModel model,
+                          MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick,
+                          int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay,
+                red, green, blue, alpha);
+        poseStack.scale(3.5F, 3.5F, 3.5F);
+    }
+
 
     @Override
     protected int getBlockLightLevel(@NotNull Mothership entity, @NotNull BlockPos blockPos) {
