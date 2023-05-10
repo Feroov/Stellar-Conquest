836a91c0efaef60aacb56b6615a1865ba4d3e583
Feroov <feroovt@gmail.com>
2023-05-10 22:56:41 +0200

    Entity registering etc

--- src/main/java/com/feroov/frv/events/EventsSTLCON.java
+++ src/main/java/com/feroov/frv/events/EventsSTLCON.java
@@ -3,6 +3,7 @@ package com.feroov.frv.events;
 import com.feroov.frv.STLCON;
 import com.feroov.frv.entity.EntitiesSTLCON;
 import com.feroov.frv.entity.monster.Celestroid;
+import com.feroov.frv.entity.monster.Mothership;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,5 +15,6 @@ public class EventsSTLCON
     public static void registerAttributes(EntityAttributeCreationEvent event)
     {
         event.put(EntitiesSTLCON.CELESTROID.get(), Celestroid.setAttributes());
+        event.put(EntitiesSTLCON.MOTHERSHIP.get(), Mothership.setAttributes());
     }
 }
