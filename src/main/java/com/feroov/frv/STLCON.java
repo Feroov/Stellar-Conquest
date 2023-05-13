05025f8848104d3eb003cc4809dde8284249cca1
Feroov <feroovt@gmail.com>
2023-05-13 13:19:03 +0200

    Setup placement registry

--- src/main/java/com/feroov/frv/STLCON.java
+++ src/main/java/com/feroov/frv/STLCON.java
@@ -7,10 +7,12 @@ import com.feroov.frv.item.ItemsSTLCON;
 import com.feroov.frv.item.RangedItems;
 import com.feroov.frv.sound.SoundEventsSTLCON;
 import com.feroov.frv.world.dimension.DimensionsSTLCON;
+import com.feroov.frv.world.placement.PlacementRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
@@ -30,8 +32,14 @@ public class STLCON
         BlocksSTLCON.register(eventBus);
         DimensionsSTLCON.register();
 
+        eventBus.addListener(this::setup);
         eventBus.addListener(this::doClientStuff);
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        PlacementRegistry.init();
     }
 
     private void doClientStuff(final FMLClientSetupEvent event)
