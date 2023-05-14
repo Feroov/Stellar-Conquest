eb636ec8fb49f55d33516da3edc6f901dc17dc7f
Feroov <feroovt@gmail.com>
2023-05-14 14:29:19 +0200

    Create a custom block for Xenosgrass Block

---
+++ src/main/java/com/feroov/frv/block/custom/XenosgrassBlock.java
@@ -0,0 +1,104 @@
+package com.feroov.frv.block.custom;
+
+import com.feroov.frv.block.BlocksSTLCON;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.PlantType;
+
+import org.jetbrains.annotations.Nullable;
+import java.util.*;
+
+public class XenosgrassBlock extends Block implements BonemealableBlock {
+
+    public XenosgrassBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        return ctx.getLevel().getBlockState(ctx.getClickedPos().above()).getMaterial().isSolid() ?
+                Blocks.DIRT.defaultBlockState() : super.getStateForPlacement(ctx);
+    }
+
+    @Override
+    public boolean canSustainPlant(BlockState state, BlockGetter getter, BlockPos pos, Direction direction, IPlantable plantable) {
+        if (direction != Direction.UP)
+            return false;
+        PlantType plantType = plantable.getPlantType(getter, pos.relative(direction));
+        return plantType == PlantType.CROP || plantType == PlantType.PLAINS || plantType == PlantType.CAVE;
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader p_256229_, BlockPos p_256432_, BlockState p_255677_, boolean p_256630_) {
+        return p_256229_.getBlockState(p_256432_.above()).isAir();
+    }
+
+
+
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource rand, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(ServerLevel p_221270_, RandomSource p_221271_, BlockPos p_221272_, BlockState p_221273_) {
+        BlockPos blockpos = p_221272_.above();
+        BlockState blockstate = Blocks.GRASS.defaultBlockState();
+        Optional<Holder.Reference<PlacedFeature>> optional = p_221270_.registryAccess().registryOrThrow(Registries.PLACED_FEATURE).getHolder(VegetationPlacements.GRASS_BONEMEAL);
+
+        label49:
+        for(int i = 0; i < 128; ++i) {
+            BlockPos blockpos1 = blockpos;
+
+            for(int j = 0; j < i / 16; ++j) {
+                blockpos1 = blockpos1.offset(p_221271_.nextInt(3) - 1, (p_221271_.nextInt(3) - 1) * p_221271_.nextInt(3) / 2, p_221271_.nextInt(3) - 1);
+                if (!p_221270_.getBlockState(blockpos1.below()).is(this) || p_221270_.getBlockState(blockpos1).isCollisionShapeFullBlock(p_221270_, blockpos1)) {
+                    continue label49;
+                }
+            }
+
+            BlockState blockstate1 = p_221270_.getBlockState(blockpos1);
+            if (blockstate1.is(blockstate.getBlock()) && p_221271_.nextInt(10) == 0) {
+                ((BonemealableBlock)blockstate.getBlock()).performBonemeal(p_221270_, p_221271_, blockpos1, blockstate1);
+            }
+
+            if (blockstate1.isAir()) {
+                Holder<PlacedFeature> holder;
+                if (p_221271_.nextInt(8) == 0) {
+                    List<ConfiguredFeature<?, ?>> list = p_221270_.getBiome(blockpos1).value().getGenerationSettings().getFlowerFeatures();
+                    if (list.isEmpty()) {
+                        continue;
+                    }
+
+                    holder = ((RandomPatchConfiguration)list.get(0).config()).feature();
+                } else {
+                    if (!optional.isPresent()) {
+                        continue;
+                    }
+
+                    holder = optional.get();
+                }
+
+                holder.value().place(p_221270_, p_221270_.getChunkSource().getGenerator(), p_221271_, blockpos1);
+            }
+        }
+
+    }
+}
