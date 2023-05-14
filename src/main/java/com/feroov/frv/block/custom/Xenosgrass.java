db98f612921b61cc4bd0152dd31a83ed1d004f5a
Feroov <feroovt@gmail.com>
2023-05-14 14:30:08 +0200

    Implementation of xenograss

---
+++ src/main/java/com/feroov/frv/block/custom/Xenosgrass.java
@@ -0,0 +1,24 @@
+package com.feroov.frv.block.custom;
+
+import com.feroov.frv.block.PlantBlocksSTLCON;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class Xenosgrass extends PlantBlocksSTLCON
+{
+    private static final VoxelShape XENOSGRASS_SHAPE = Block.box(3, 0, 3, 13, 14, 13);
+
+    public Xenosgrass(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context)
+    {
+        return XENOSGRASS_SHAPE;
+    }
+}
