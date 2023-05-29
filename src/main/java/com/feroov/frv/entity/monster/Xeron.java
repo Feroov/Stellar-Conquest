ad975f380d6df7366ffaa66a7984bde3c3370c5c
Feroov <feroovt@gmail.com>
2023-05-29 22:09:33 +0200

    Remove unused import

--- src/main/java/com/feroov/frv/entity/monster/Xeron.java
+++ src/main/java/com/feroov/frv/entity/monster/Xeron.java
@@ -8,7 +8,6 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.Difficulty;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
