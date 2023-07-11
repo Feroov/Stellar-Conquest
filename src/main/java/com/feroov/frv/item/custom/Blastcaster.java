b0181a846107768d18e210cd8b934306cd6e75d3
Feroov <feroovt@gmail.com>
2023-07-11 22:29:34 +0200

    Finalize Blastcaster

--- src/main/java/com/feroov/frv/item/custom/Blastcaster.java
+++ src/main/java/com/feroov/frv/item/custom/Blastcaster.java
@@ -15,6 +15,8 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
