600cf5dc007bf20f570cd2042cc2aab3b776169f
Feroov <feroovt@gmail.com>
2023-07-29 09:42:48 +0200

    BossSpawnerBlock JavaDocs

--- src/main/java/com/feroov/frv/block/BossSpawnerBlock.java
+++ src/main/java/com/feroov/frv/block/BossSpawnerBlock.java
@@ -21,25 +21,65 @@ import org.jetbrains.annotations.Nullable;
 
 public class BossSpawnerBlock extends BaseEntityBlock
 {
+    // The voxel shape of the boss spawner block.
     private static final VoxelShape SHAPE = Block.box(-4, -4, -4, 10, 10, 10);
+
+    // The variant of the boss associated with this spawner block.
     private final BossVariant boss;
 
+    /**
+     * Constructs a BossSpawnerBlock with the specified properties and boss variant.
+     *
+     * @param props The properties for the BossSpawnerBlock.
+     * @param variant The boss variant associated with this spawner block.
+     */
     public BossSpawnerBlock(BlockBehaviour.Properties props, BossVariant variant)
     {
         super(props);
         this.boss = variant;
     }
 
+    /**
+     * Gets the render shape for this block.
+     *
+     * @param state The current state of the block.
+     * @return The RenderShape.MODEL indicating this block uses the model for rendering.
+     */
     @Override
     public RenderShape getRenderShape(BlockState state) { return RenderShape.MODEL; }
 
+    /**
+     * Creates a new BlockEntity instance for this block at the specified position.
+     *
+     * @param pos The position of the block in the world.
+     * @param state The current state of the block.
+     * @return The new BossSpawnerBlockEntity instance associated with this block.
+     */
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) { return boss.getType().create(pos, state); }
 
+    /**
+     * Gets the VoxelShape of this block's collision box.
+     *
+     * @param state The current state of the block.
+     * @param getter The BlockGetter to use for retrieving neighboring blocks' information.
+     * @param pos The position of the block in the world.
+     * @param context The CollisionContext to use for collision calculations.
+     * @return The VoxelShape representing this block's collision box.
+     */
     @Override
     public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) { return SHAPE; }
 
+    /**
+     * Gets the BlockEntityTicker for this block, used for updating block entities in the world.
+     *
+     * @param level The Level where the block entity is placed.
+     * @param state The current state of the block.
+     * @param type The BlockEntityType of the block entity.
+     * @param <T> The type of BlockEntity associated with this block.
+     * @return The BlockEntityTicker instance for the boss spawner block entity.
+     */
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type)
@@ -47,6 +87,15 @@ public class BossSpawnerBlock extends BaseEntityBlock
         return createTickerHelper(type, boss.getType(), BossSpawnerBlockEntity::tick);
     }
 
+    /**
+     * Determines whether the given entity can destroy this block.
+     *
+     * @param state The current state of the block.
+     * @param getter The BlockGetter to use for retrieving neighboring blocks' information.
+     * @param pos The position of the block in the world.
+     * @param entity The entity trying to destroy the block.
+     * @return True if the entity can destroy the block, false otherwise.
+     */
     @Override
     public boolean canEntityDestroy(BlockState state, BlockGetter getter, BlockPos pos, Entity entity)
     {
